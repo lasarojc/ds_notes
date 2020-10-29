47ca3d92b273a28ad56121b135d371e458f465c7
Lasaro Camargos <lcamargos@commvault.com>
2020-10-29 11:49:52 -0300

    Deployed 6de481e with MkDocs version: 1.1

--- basics/code/server.py
+++ basics/code/server.py
@@ -4,7 +4,7 @@
 import socket                                   # Import socket module
 
 s = socket.socket()                             # Create a socket object
-host = socket.gethostname()                # Get local machine name
+host = "0.0.0.0"
 port = 12345                                    # Reserve a port for your service.
 s.bind((host, port))                            # Bind to the port
 
@@ -12,5 +12,5 @@ s.listen(5)                                     # Now wait for client connection
 while True:
    c, addr = s.accept()                         # Establish connection with client.
    print('Got connection from', addr)
-   c.send('Thank you for connecting'.encode())
+   c.send("teste".encode())
    c.close() 
